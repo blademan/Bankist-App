846f448c69408ccc7b090b4befadb489aa493dfe
Eduards Leimanis <eduards.leimanis@gmail.com>
2021-01-22 05:43:50 +0200

    to fix numbers

--- script.js
+++ script.js
@@ -4,15 +4,26 @@
 /////    BANKIST APP  /////////////////
 ///////////////////////////////////////
 
-////////////////////////////////
-////// Data  ///////////////////
-////////////////////////////////
+// DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -20,25 +31,22 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const accounts = [account1, account2];
 
 ////////////////////////////////
 ////// Elements  ///////////////
@@ -86,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
       i + 1
     } ${type}</div>
           
-          <div class="movements__value">${mov}€</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -99,7 +107,7 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}`;
 };
 
 //////////////////////////////////////
@@ -110,19 +118,19 @@ const calcDisplaySummary = function (curentAcc) {
   const incomes = curentAcc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const outcomes = curentAcc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+  labelSumOut.textContent = `${Math.abs(outcomes.toFixed(2))}€`;
 
   const interest = curentAcc.movements
     .filter((mov) => mov > 0)
     .map((mov) => (mov * curentAcc.interestRate) / 100)
     .filter((mov) => mov >= 1)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}.€`;
 };
 
 ////////////////////////////////////
@@ -237,7 +245,7 @@ btnClose.addEventListener('click', (e) => {
 
 btnLoan.addEventListener('click', (e) => {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   const loan = currentAccount.movements.some((mov) => {
     return mov >= amount * 0.1;
   });
@@ -263,3 +271,27 @@ btnSort.addEventListener('click', (e) => {
 //////////////////////////////////
 ////////////    LECTURE  /////////
 //////////////////////////////////
+
+// console.log(Number.parseInt('30px'));
+// console.log(Number.parseFloat('2.5px'));
+
+// console.log(Number.isNaN(22));
+// console.log(Number.isFinite(+'1'));
+
+// console.log(Number.isInteger(2));
+
+// console.log(Math.max(3, 5, 8, 9, 23));
+// console.log(Math.max(3, 5, 8, '9', 23));
+// console.log(Math.max(3, 5, 8, '2px', 23));
+
+// console.log(Math.min(3, 5, 8, 9, 23));
+
+// console.log(Math.trunc(2.33));
+
+// const randomInt = (min, max) =>
+//   Math.trunc(Math.random() * (max - min) + 1 + min);
+
+// console.log(randomInt(1, 5));
+console.log(Math.floor(2.3));
+console.log(Math.trunc(2.3));
+console.log((2.333).toFixed(1));
